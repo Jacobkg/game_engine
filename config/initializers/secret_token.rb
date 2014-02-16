a0d6ceffbecdb1b868a62234efaa078ba492f2a8
Jacob Green <jacob@nationbuilder.com>
2014-02-15 21:55:07 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyProject::Application.config.secret_key_base = 'fe28866e1feb00b1c8749735d87c8862e78e7f64c3127cb9326399442a772c62f6e76896339dac58cf5043488505555a6494ff2d10a9921702c89b20d7d1ea78'
