8ceb478272ea71ffbafea98f7e36ef6ee90775fc
Jacob Green <jacob@nationbuilder.com>
2014-02-17 16:23:32 -0500

    Change the way we store the board.

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,16 +2,19 @@ class Board
 
   GRID_SIZE = 20
 
-  attr_reader :board, :player_to_move, :x_score, :y_score
-  def initialize(board, player_to_move, x_score, y_score)
-    @board = board
-    @player_to_move = player_to_move
+  attr_reader :game_id, :player_to_move, :x_score, :y_score, :x_position, :y_position
+  def initialize(game_id, stars, x_position, y_position, x_score, y_score, player_to_move)
+    @game_id = game_id
+    @stars = stars
+    @x_position = x_position
+    @y_position = y_position
     @x_score = x_score
     @y_score = y_score
+    @player_to_move = player_to_move
   end
 
   def self.create!
-    board = Board.new(generate_random_board, "X", 0, 0)
+    board = Board.new(1, generate_random_stars, [0,0], [GRID_SIZE - 1, GRID_SIZE - 1], 0, 0, "X")
     board.save!
     board
   end
@@ -19,12 +22,13 @@ class Board
   def self.load
     return nil unless File.exists?(".board")
     board_json = JSON.parse(File.read('.board'))
-    board = board_json["board"].lines.map {|line| line.strip.split("")}
-    Board.new(board, board_json["player"], board_json["x_score"], board_json["y_score"])
+    Board.new(board_json["game_id"], board_json["stars"], board_json["x_position"], board_json["y_position"],
+              board_json["x_score"], board_json["y_score"], board_json["player_to_move"])
   end
 
   def save!
-    board_json = { board: display_string, player: player_to_move, x_score: x_score, y_score: y_score }.to_json
+    board_json = { game_id: game_id, stars: @stars, x_position: x_position, y_position: y_position,
+                   x_score: x_score, y_score: y_score, player_to_move: player_to_move }.to_json
     File.open('.board', 'w') {|file| file.write(board_json)}
   end
 
@@ -40,11 +44,11 @@ class Board
       new_x, new_y = board_x - 1, board_y
     end
     if legal_move?(new_x, new_y)
-      if @board[new_x][new_y] == "*"
+      if @stars.include?([new_x, new_y])
         player_to_move == "X" ? @x_score += 1 : @y_score += 1
+        @stars.delete([new_x, new_y])
       end
-      @board[board_x][board_y] = "-"
-      @board[new_x][new_y] = player_to_move
+      update_player_position(new_x, new_y)
     end
     @player_to_move = other_player
     save!
@@ -54,34 +58,48 @@ class Board
     board_output = ""
     0.upto(GRID_SIZE - 1) do |i|
       0.upto(GRID_SIZE - 1) do |j|
-        board_output << @board[i][j]
+        if [i,j] == x_position
+          board_output << "X"
+        elsif [i,j] == y_position
+          board_output << "Y"
+        elsif @stars.include?([i,j])
+          board_output << "*"
+        else
+          board_output << "-"
+        end
       end
       board_output << "\n"
     end
     board_output
   end
 
-  def self.generate_random_board
-    board = Array.new(GRID_SIZE) { Array.new(GRID_SIZE) { "-" } }
+  def self.generate_random_stars
+    star_list = []
     GRID_SIZE.times do
       rand_x = rand(GRID_SIZE)
       rand_y = rand(GRID_SIZE)
-      board[rand_x][rand_y] = "*"
+      star_list << [rand_x, rand_y]
     end
-    board[0][0] = "X"
-    board[GRID_SIZE - 1][GRID_SIZE - 1] = "O"
-    board
+    star_list.uniq
   end
 
   def player_coordinates(player)
-    0.upto(GRID_SIZE - 1) do |i|
-      0.upto(GRID_SIZE - 1) do |j|
-        return [i,j] if @board[i][j] == player
-      end
+    if player == "X"
+      x_position
+    else
+      y_position
     end
   end
 
   private
+
+    def update_player_position(new_x, new_y)
+      if player_to_move == "X"
+        @x_position = [new_x, new_y]
+      else
+        @y_position = [new_x, new_y]
+      end
+    end
 
     def legal_move?(new_x, new_y)
       return false if new_x < 0 || new_y < 0 || new_x >= GRID_SIZE || new_y >= GRID_SIZE
@@ -90,7 +108,7 @@ class Board
     end
 
     def other_player
-      player_to_move == "X" ? "O" : "X"
+      player_to_move == "X" ? "Y" : "X"
     end
 
 end
